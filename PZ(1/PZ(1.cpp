56a1df8a4b08e290aa860936e621d3dbef152e06
User <User@DESKTOP-HT7QH2S>
2025-09-01 12:02:56 +0300

    Добавьте файлы проекта.

---
+++ PZ(1/PZ(1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    system("color E1");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    // Прізвище
+    string last_name;
+    cout << "Введіть прізвище: ";
+    getline(cin, last_name);
+
+    // Ім'я
+    string first_name;
+    cout << "Введіть ім'я: ";
+    getline(cin, first_name);
+
+    // Вік
+    int age;
+    cout << "Введіть вік: ";
+    cin >> age;
+
+    // Очищення буфера після введення числа
+    cin.ignore();
+
+    // Нові запитання
+    string city;
+    cout << "Введіть місто, де ви живете: ";
+    getline(cin, city);
+
+    string subject;
+    cout << "Введіть улюблений предмет: ";
+    getline(cin, subject);
+
+    string hobby;
+    cout << "Введіть улюблене хобі: ";
+    getline(cin, hobby);
+
+    // Вивід введених даних
+    cout << "\nВаші дані:" << endl;
+    cout << "Прізвище: " << last_name << endl;
+    cout << "Ім'я: " << first_name << endl;
+    cout << "Вік: " << age << endl;
+    cout << "Місто: " << city << endl;
+    cout << "Улюблений предмет: " << subject << endl;
+    cout << "Хобі: " << hobby << endl;
+
+    return 0;
+}
